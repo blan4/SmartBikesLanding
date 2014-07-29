b6e8d7bd910fa61e2fc692e1170b266bd5bfbc4c
ilya <ilya.blan4@gmail.com>
2014-07-29 15:51:14 +0700

    add form

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,1 +1,21 @@
-console.log('lol')
+$(document).on('submit', 'form#js-subscribe', function(ev) {
+  ev.preventDefault();
+  var $email = $("input[name='email']");
+  var $respBox = $("#responseBox");
+  var data = {email: $email.val()};
+  $.ajax({
+    type: 'POST',
+    url: '/subscriptions',
+    data: JSON.stringify(data),
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function(res) {
+      if (res && !res.error) {
+        $email.val(''); 
+        $respBox.html("<p>Success</p>");
+      } else {
+        $respBox.html("<p>"+res.error+"</p>");
+      }
+    }
+  });
+});
