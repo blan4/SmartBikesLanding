5879ea20997f5610d3aec78e09206506a7e7d99d
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-06 15:21:40 +0700

    fix vk sharing

--- js/likes.js
+++ js/likes.js
@@ -1,11 +1,16 @@
 (function($){
-  $(document).ready(function(){
-    $("#vkLike").html(VK.Share.button({
-      url: 'http://smart-bikes.ru',
-      noparse: false
-    }, {
-      type: 'custom',
-      text: '<img src="http://vk.com/images/vk32.png" />'
-    }));
-  });
+  window.Share = {
+    vk: function(url, title, description, img) {
+      url  = 'http://vkontakte.ru/share.php?';
+      url += 'url='          + encodeURIComponent(url);
+      url += '&title='       + encodeURIComponent(title);
+      url += '&description=' + encodeURIComponent(description);
+      url += '&image='       + encodeURIComponent(img);
+      url += '&noparse=true';
+      Share.popup(url);
+    },
+    popup: function(url) {
+      window.open(url,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+    }
+  }
 })(jQuery);
