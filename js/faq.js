95fc3451f33d5d26a678ab6fba88cb443f098b00
Ilya Siganov <ilya.blan4@gmail.com>
2014-08-21 17:10:41 +0700

    add some style

--- js/faq.js
+++ js/faq.js
@@ -1,12 +1,17 @@
 (function($){
   $(document).ready(function(){
-    $('.answer').css('display', 'none');
+    $('.answer').hide();
 
     $(document).on('click', '.question', function(event) {
       event.preventDefault();
-      $('.answer').css('display', 'none');
       var $self = $(this);
-      $('.answer[data-answer='+ $self.data('question') +']').css('display', 'block');
+      var $answer = $('.answer[data-answer='+ $self.data('question') +']');
+      if (!$answer.is(':hidden')) {
+        $answer.hide();
+      } else {
+        $('.answer').hide();
+        $answer.show();
+      }
     });
   });
 })(jQuery);
